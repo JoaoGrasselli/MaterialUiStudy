4ad4a6a6ace3ad123df4cfd73009a291bd0e6fed
joao-grasselli <jgrasseli@ext.leroymerlin.com.br>
2024-05-31 17:14:36 -0300

    feat: customizing the document title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
 
 function App() {
+  useEffect(() => {
+    document.title = "Gerenciamento de Projetos - João Grasselli";
+  });
+
   return (
     <div className="App">
       <Header />
